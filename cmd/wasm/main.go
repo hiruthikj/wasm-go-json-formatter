1c54712f3675b8183c9245472d883301f9458eb1
Hiruthik J <hiruthik27+github@gmail.com>
2025-10-25 17:19:29 +0530

    chore

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -8,10 +8,11 @@ import (
 	"syscall/js"
 )
 
+// Build with GOOS=js GOARCH=wasm go build -o ../../assets/json.wasm -a
 func main() {
 	fmt.Println("Hello wasm!1")
-	js.Global().Set("formatJSON", jsonWrapper())
-	<-make(chan struct{})
+	js.Global().Set("formatJSON", js.FuncOf(jsonWrapper))
+	select {}
 }
 
 func prettyJson(input string) (string, error) {
@@ -27,30 +28,26 @@ func prettyJson(input string) (string, error) {
 	return string(pretty), nil
 }
 
-func jsonWrapper() js.Func {
-	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
-		if len(args) != 1 {
-			return []any{nil, "invalid no. of arguments passed"}
-		}
-		document := js.Global().Get("document")
-		if !document.Truthy() {
-			return []any{nil, "unable to get document object"}
-		}
-		jsonOutputBox := document.Call("getElementById", "jsonoutput")
-		if !jsonOutputBox.Truthy() {
-			return []any{nil, "unable to get json output text area"}
-		}
+func jsonWrapper(this js.Value, args []js.Value) any {
+	if len(args) != 1 {
+		return []any{nil, "invalid no. of arguments passed"}
+	}
+	document := js.Global().Get("document")
+	if !document.Truthy() {
+		return []any{nil, "unable to get document object"}
+	}
+	jsonOutputBox := document.Call("getElementById", "jsonoutput")
+	if !jsonOutputBox.Truthy() {
+		return []any{nil, "unable to get json output text area"}
+	}
 
-		input := args[0].String()
-		fmt.Printf("input: %v\n", input)
-		pretty, err := prettyJson(input)
-		if err != nil {
-			errStr := fmt.Sprintf("unable to convert to json %s\n", err)
-			return []any{nil, errStr}
-		}
-		jsonOutputBox.Set("value", pretty)
-		return []any{pretty, nil}
-	})
-
-	return jsonFunc
+	input := args[0].String()
+	fmt.Printf("input: %v\n", input)
+	pretty, err := prettyJson(input)
+	if err != nil {
+		errStr := fmt.Sprintf("unable to convert to json %s\n", err)
+		return []any{nil, errStr}
+	}
+	jsonOutputBox.Set("value", pretty)
+	return []any{pretty, nil}
 }
