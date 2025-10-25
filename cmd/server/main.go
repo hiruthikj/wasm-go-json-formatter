b4bb89c1459ce37b488ec5ca4a11ab97e3ad550f
Hiruthik J <hiruthik27+github@gmail.com>
2025-10-25 16:24:15 +0530

    Open server url in browser

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/pkg/browser"
 )
 
 func main() {
 	port := ":9090"
-	fmt.Println("Starting Server in http://localhost" + port)
+	url := "http://localhost" + port
+	fmt.Println("Starting Server in", url)
+	browser.OpenURL(url)
+
 	if err := http.ListenAndServe(port, http.FileServer(http.Dir("../../assets"))); err != nil {
 		fmt.Println("Failed to start server, err: ", err)
 	}
